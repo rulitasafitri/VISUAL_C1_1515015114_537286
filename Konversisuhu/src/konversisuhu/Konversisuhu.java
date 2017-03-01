fa7b3600a0551540847f4b9fe25325e6d6a712cf
Rulita Safitri <Rulita.safitri97@gmail.com>
2017-02-28 20:21:35 -0800

    posttest1

---
+++ Konversisuhu/src/konversisuhu/Konversisuhu.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konversisuhu;
+
+/**
+ *
+ * @author USER
+ */
+public class Konversisuhu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Latihan2().setVisible(true);
+            }
+        });
+    }
+    
+}
